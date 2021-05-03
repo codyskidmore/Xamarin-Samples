4b9f0190a849ccc7deadf80dde01b3f444fa091f
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2021-05-03 10:12:01 -0400

    Added a button example.

---
+++ ButtonTutorial/ButtonTutorial/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ButtonTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly Random _randomGen = new Random();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            ((Button) sender).Text = $"Click me again! {_randomGen.Next(1,100)}";
+        }
+    }
+}
