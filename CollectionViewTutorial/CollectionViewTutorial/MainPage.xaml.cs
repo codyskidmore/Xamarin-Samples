ad8203f55c8d8575acc0c786c8014bd0c5383c48
DESKTOP-4V1NKRS\Cody Skidmore <nospam@skidmore.us>
2021-05-08 13:58:42 -0400

    Added a CollectionView example.

---
+++ CollectionViewTutorial/CollectionViewTutorial/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CollectionViewTutorial
+{
+    public partial class MainPage : ContentPage
+    {
+        public ICollection<Monkey> Monkeys { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Monkeys = Repository.GetMonkeys();
+            BindingContext = this;
+        }
+
+        private void SelectableItemsView_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Monkey selectedItem = e.CurrentSelection[0] as Monkey;
+        }
+    }
+}
